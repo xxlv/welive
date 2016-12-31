bd9cde8322cbe0b34dc970d9b96024f32450b3d6
x <lvxx@dxyer.com>
2016-12-31 18:16:04 +0800

    Add websocket

--- pages/live/live.js
+++ pages/live/live.js
@@ -5,18 +5,39 @@ var liveData=[]
 
 var util = require('../../utils/util.js')
 
+
+wx.connectSocket({
+    url: 'ws://127.0.0.1:8080'
+})
+wx.onSocketOpen(function(){
+    console.log('Socket open')
+})
+
+
+wx.onSocketMessage(function(res) {
+
+    var data=JSON.parse(res.data)
+
+    liveData.push(
+        {
+          nick:data.nick,
+          message:data.message,
+          date:data.date
+      })
+})
+
+
 Page({
   data: {
     liveData:liveData
   },
-
   bindViewTap: function() {
     wx.navigateTo({
       url: '../live/live'
+
     })
   },
   onLoad: function () {
-    console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
@@ -27,22 +48,15 @@ Page({
   });
 
   setInterval(function(){
-      console.log(liveData)
-      liveData.push({
-          nick:"Ghost",
-          message:"发言数据这里"+Math.random(1,100000),
-          date:util.formatTime(new Date())
-      })
 
       if(liveData.length>44){
           liveData=[]
       }
-
       that.setData({
           liveData:liveData
       })
-
-
   },2333)
+
+
   }
 })
